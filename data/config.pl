372f30b014f55ed6fcc5036bda1bf6d4738510d5
Andrew <0685870650a@gmail.com>
2019-07-05 15:48:46 +0300

    teeest

--- data/config.pl
+++ data/config.pl
@@ -5,6 +5,6 @@
   DB_NAME     => 'discount_catalog',
   DB_USER     => 'dc',
   DB_PASSWORD => 'dc',
-#   CHAT_IP     => '46.175.141.27'
+  CHAT_IP     => '46.175.141.27'
   CHAT_IP     => '192.168.1.110'
 );
